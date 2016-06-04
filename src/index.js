85521491c95e238ed1ae2aded7541262c738b7f5
Julien Enselme <julien.enselme@centrale-marseille.fr>
2016-06-04 15:20:30 +0200

    style(index): add eslintrc and correct style

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ export function configure(config) {
     let ea = container.get(EventAggregator);
     let firstPage = true;
 
-    if (window['_paq']) {
+    if (window._paq) {
         ea.subscribe('router:navigation:success', () => {
             // The first visited page is logged by the tracking code supplied by piwik.
             if (!firstPage) {
-                _paq.push(['trackPageView', location.pathname]);
+                window._paq.push(['trackPageView', location.pathname]);
             } else {
                 firstPage = false;
             }
         });
     } else {
-        console.warn('piwik module is loaded but no piwik _paq object was found');
+        console.warn('piwik module is loaded but no piwik _paq object was found');  // eslint-disable-line
     }
 }
